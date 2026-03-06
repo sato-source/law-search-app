0a756b3a398d4cc0e7d54401c74c2319f7f442d1
sato-source <sato_t@againg.co.jp>
2026-03-06 11:18:45 +0900

    Update law.js

--- api/law.js
+++ api/law.js
@@ -1,19 +1,14 @@
-import { getLawData } from "../backend/egov.js";
+const { getLawData } = require("../backend/egov.js");
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   try {
     const { id } = req.query;
-
     const data = await getLawData(id);
-
     res.status(200).json(data);
-
   } catch (e) {
-
     console.error(e);
-
     res.status(500).json({
       error: String(e)
     });
   }
-}
+};

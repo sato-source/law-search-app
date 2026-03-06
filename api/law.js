129a27485b6d1e6ba360fc76ca10425ae4bb3c3b
sato-source <sato_t@againg.co.jp>
2026-03-06 10:15:05 +0900

    Create law.js

---
+++ api/law.js
@@ -0,0 +1,11 @@
+import { getLawData } from "../backend/egov.js";
+
+export default async function handler(req, res) {
+  try {
+    const { id } = req.query;
+    const data = await getLawData(id);
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(500).json({ error: String(e) });
+  }
+}
